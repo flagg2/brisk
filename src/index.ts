397337079f0ede0cc799f8140e97c21e712ce7ab
flagg2 <samo.wittlinger@gmail.com>
2022-12-21 15:43:26 +0100

    fix indeX

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import { Role } from "./server/Auth";
 import { Brisk } from "./server/Brisk";
 
-const x = new Brisk({
-   port: 3000,
-});
-
-x.get("/", (req, res) => {
-   return res.ok({
-      en: "Hello world",
-      sk: "Ahoj svet",
-   });
-});
-
-x.start();
-
 export { Role, Brisk };
