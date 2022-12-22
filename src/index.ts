702132ffc9f2ea1a5a782de69ca7e55c3ab6963c
flagg2 <samo.wittlinger@gmail.com>
2022-12-22 14:33:56 +0100

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
 import { Role } from "./server/Auth";
 import { Brisk } from "./server/Brisk";
-import zod from "zod";
 
 export { Role, Brisk };
 
 const x = new Brisk({
    port: 3000,
 });
-
-x.get(
-   "/test",
-   (req, res) => {
-      req.body;
-      return res.ok("Hello World");
-   },
-   {
-      validation: {
-         schema: zod.object({
-            name: zod.string(),
-         }),
-         isStrict: false,
-      },
-   }
-);
