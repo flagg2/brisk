b0264cfbf097ed3ced4bf4904ea0f54cd5d465aa
flagg2 <samo.wittlinger@gmail.com>
2023-02-06 12:31:13 +0100

    Remove ignored files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Brisk = exports.Role = void 0;
-const Auth_1 = require("./server/Auth");
-Object.defineProperty(exports, "Role", { enumerable: true, get: function () { return Auth_1.Role; } });
-const Brisk_1 = require("./server/Brisk");
-Object.defineProperty(exports, "Brisk", { enumerable: true, get: function () { return Brisk_1.Brisk; } });
-//# sourceMappingURL=index.js.map
